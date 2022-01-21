88de841547bbcfb1eaad21de95109049bbc37018
shehab20089 <shehabmohsen1995@gmail.com>
2022-01-21 14:44:13 +0200

    fixing a small bug in attribute selector

--- src/components/ِAttributeSelector/AttributeSelector.jsx
+++ src/components/ِAttributeSelector/AttributeSelector.jsx
@@ -12,22 +12,30 @@ export default class AttributeSelector extends Component {
     super(props);
     this.state = { selectedAttribute: this.props.attribute.selectedItem };
   }
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevProps.attribute.selectedItem.id !==
+      this.props.attribute.selectedItem.id
+    )
+      this.setState({ selectedAttribute: this.props.attribute.selectedItem });
+  }
   handleItemChange = (item) => {
     this.setState({ selectedAttribute: item });
     this.props.onAttributeChange(item, this.props.attribute.id);
   };
   render() {
-    const { name, items, type } = this.props.attribute;
+    const { name, items, type, selectedItem } = this.props.attribute;
+    const { size } = this.props;
     const { selectedAttribute } = this.state;
 
     return (
       <AttributeContainer>
-        <AttributeTitle>{name}:</AttributeTitle>
-
+        <AttributeTitle size={size}>{name}:</AttributeTitle>
         <AttributeSelection>
           {items.map((item) => {
             return type != "swatch" ? (
               <AttributeSelectionItem
+                size={size}
                 selected={selectedAttribute.value == item.value}
                 onClick={() => this.handleItemChange(item)}
                 key={item.id}
@@ -39,7 +47,10 @@ export default class AttributeSelector extends Component {
                 color={item.value}
                 selected={selectedAttribute.value == item.value}
                 onClick={() => this.handleItemChange(item)}
-                size={{ height: "40px", width: "40px" }}
+                size={{
+                  height: size != "mini" ? "45px" : "24px",
+                  width: size != "mini" ? "45px" : "24px",
+                }}
                 key={item.id}
               ></AttributeSelectionItemSwatch>
             );
--- src/components/ِAttributeSelector/AttributeSelector.styles.js
+++ src/components/ِAttributeSelector/AttributeSelector.styles.js
@@ -2,18 +2,23 @@ import styled from "styled-components";
 
 export const AttributeContainer = styled.div``;
 export const AttributeTitle = styled.p`
-  font-weight: 700;
+  font-weight: ${({ size }) => (size != "mini" ? "700" : "300")};
+  font-size: ${({ size }) => (size != "mini" ? "1rem" : "0.9rem")};
   text-transform: uppercase;
 `;
 export const AttributeSelection = styled.div`
   display: flex;
-  gap: 12px;
+
+  gap: ${({ size }) => (size != "mini" ? "12px" : "5px")};
 `;
 export const AttributeSelectionItem = styled.div`
   border: 1px solid #1d1f22;
-  width: 63px;
-  height: 45px;
-  font-size: 0.9rem;
+
+  min-width: ${({ size }) => (size != "mini" ? "63px" : "25px")};
+  min-height: ${({ size }) => (size != "mini" ? "45px" : "25px")};
+  font-size: ${({ size }) => (size != "mini" ? "0.9rem" : "0.5rem")};
+  /* padding: ${({ size }) => (size != "mini" ? "0" : "0.3rem")}; */
+
   display: flex;
   justify-content: center;
   font-family: "Source Sans Pro", sans-serif;
