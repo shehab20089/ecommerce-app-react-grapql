84eba015fdd0a33ae1b5bcbe1d72d3530b4e6abf
shehab20089 <shehabmohsen1995@gmail.com>
2022-01-21 14:43:32 +0200

    modify basebutton

--- src/components/Base/BaseButton.styles.js
+++ src/components/Base/BaseButton.styles.js
@@ -3,13 +3,16 @@ import styled from "styled-components";
 export const BaseButton = styled.button`
   cursor: pointer;
   height: ${({ size }) => size.height};
+  width: 100%;
   border: none;
   background: none;
   background-color: ${({ bgColor }) =>
     bgColor ? bgColor : "var(--clr-primary)"};
+  border: ${({ border }) => (border ? border : "none")};
   font-weight: 600;
   font-size: ${({ size }) => size.font};
   color: ${({ color }) => (color ? color : "white")};
+  font-family: inherit;
   text-transform: uppercase;
   &:hover {
     opacity: 0.8;
